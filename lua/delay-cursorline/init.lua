0693cd1a18a52c686580b6d8c6374b78197cb643
go-toh <kurumiallergy@gmail.com>
2022-09-02 23:32:25 +0900

    Convert tabs to spaces.

--- lua/delay-cursorline/init.lua
+++ lua/delay-cursorline/init.lua
@@ -8,36 +8,36 @@ local M = {}
 local opts = {}
 
 local DEFAULT_OPTS = {
-	delay = {
-		enable = true,
-		time = 500,
-	}
+  delay = {
+    enable = true,
+    time = 500,
+  }
 }
 
 local delay_cursorline = function()
-	local current_line = get_currentline(0)[1]
-	if prev_line ~= current_line then
-		wo.cursorlineopt = "number"
-		timer:start(
-			opts.delay.time,
-			0,
-			vim.schedule_wrap(function()
-				wo.cursorlineopt = "number,line"
-			end)
-		)
-	end
-	prev_line = current_line
+  local current_line = get_currentline(0)[1]
+  if prev_line ~= current_line then
+    wo.cursorlineopt = "number"
+    timer:start(
+      opts.delay.time,
+      0,
+      vim.schedule_wrap(function()
+        wo.cursorlineopt = "number,line"
+      end)
+    )
+  end
+  prev_line = current_line
 end
 
 function M.setup(user_opts)
-	opts = vim.tbl_deep_extend("force", DEFAULT_OPTS, user_opts or {})
-	if opts.delay.enable then
-		wo.cursorline = true
-        prev_line = get_currentline(0)[1]
-		create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-			callback = delay_cursorline,
-		})
-	end
+  opts = vim.tbl_deep_extend("force", DEFAULT_OPTS, user_opts or {})
+  if opts.delay.enable then
+    wo.cursorline = true
+    prev_line = get_currentline(0)[1]
+    create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      callback = delay_cursorline,
+    })
+  end
 end
 
 return M
